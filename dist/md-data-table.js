fab15ffe1b8fadf1ce7227ad79d8738969710c1d
Eduardo Aleixo <eduardoaleixomartins@gmail.com>
2016-07-21 11:13:20 -0400

    updates angular material to 1.1

--- dist/md-data-table.js
+++ dist/md-data-table.js
@@ -2,7 +2,7 @@
  * Angular Material Data Table
  * https://github.com/daniel-nagy/md-data-table
  * @license MIT
- * v0.10.13
+ * v0.10.15
  */
 (function (window, angular, undefined) {
 'use strict';
@@ -96,7 +96,7 @@ angular.module('md-table-pagination.html', []).run(['$templateCache', function($
     '<div class="page-select" ng-if="$pagination.showPageSelect()">\n' +
     '  <div class="label">{{$pagination.label.page}}</div>\n' +
     '\n' +
-    '  <md-select virtual-page-select total="{{$pagination.pages()}}" class="md-table-select" ng-model="$pagination.page" md-container-class="md-pagination-select" ng-change="$pagination.onPaginationChange()" ng-disabled="$pagination.disabled" aria-label="Page">\n' +
+    '  <md-select virtual-page-select placeholder="page" total="{{$pagination.pages()}}" class="md-table-select" ng-model="$pagination.page" md-container-class="md-pagination-select" ng-change="$pagination.onPaginationChange()" ng-disabled="$pagination.disabled" aria-label="Page">\n' +
     '    <md-content>\n' +
     '      <md-option ng-repeat="page in $pageSelect.pages" ng-value="page">{{page}}</md-option>\n' +
     '    </md-content>\n' +
